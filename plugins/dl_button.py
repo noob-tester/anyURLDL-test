3166af7d33879f04167cb357f540b954be7a3483
Esther-Lopez <66007532+Esther-Lopez@users.noreply.github.com>
2020-07-08 14:57:01 +0530

    Update dl_button.py

--- plugins/dl_button.py
+++ plugins/dl_button.py
@@ -268,11 +268,9 @@ async def download_coroutine(bot, session, url, file_name, chat_id, message_id, 
         await bot.edit_message_text(
             chat_id,
             message_id,
-            text="""Initiating Download....
-            
-🔗URL: {}
-
-📁File Size: {}""".format(url, humanbytes(total_length))
+            text="""Initiating Download
+URL: {}
+File Size: {}""".format(url, humanbytes(total_length))
         )
         with open(file_name, "wb") as f_handle:
             while True:
@@ -291,21 +289,14 @@ async def download_coroutine(bot, session, url, file_name, chat_id, message_id, 
                         (total_length - downloaded) / speed) * 1000
                     estimated_total_time = elapsed_time + time_to_completion
                     try:
-                       current_message = """**Download Status**                    
-
-🔗URL: {}
-
-📁 Total Size : {}
-
-📥 Downloaded : {}
-
-🐌 Speed : {}
-
-⏱ Time Left : {}""".format(
+                        current_message = """**Download Status**
+URL: {}
+File Size: {}
+Downloaded: {}
+ETA: {}""".format(
     url,
     humanbytes(total_length),
     humanbytes(downloaded),
-    humanbytes(speed),                      
     TimeFormatter(estimated_total_time)
 )
                         if current_message != display_message:
